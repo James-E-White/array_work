28fd8e8c76a9e6f72c4fa71d13d2da1ad37a9f1a
James White <whitejaad@gmail.com>
2022-07-10 11:06:35 -0700

    Arrays rb

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+friends = "Allan","Joel","Wes","Shane","Jon"
+
+age_of_friends = 38,36,45,45,38
+
+time_friends = 18.5,14.1,13.3,34.5,38.7
+
+live_in_Idaho= "Shane"
+live_in_California="Joel", Allan"
+live_in_Oregon="Wes"
+live_in_Arizona="Jon"
+
+code_name = "Brothers by choice"
+puts code name
